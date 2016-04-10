6ad5151eb31509d9e71ac5cc141fdd0c3ecd32f3
Aaron Shekey <aaronshekey@github.com>
2016-04-10 12:47:03 -0500

    Menu polishing

--- src/_/js/menu.js
+++ src/_/js/menu.js
@@ -1,7 +1,9 @@
 if ( 'querySelector' in document && 'addEventListener' in window ) {
+  var menuContainer = document.getElementsByClassName('menu')[0];
   var menuBtn = document.getElementsByClassName('menu-btn')[0];
   var navigation = document.getElementsByClassName('navigation')[0];
   menuBtn.addEventListener('click', function(e) {
+    menuContainer.classList.toggle('open');
     menuBtn.classList.toggle('open');
     navigation.classList.toggle('open');
   }, false);
